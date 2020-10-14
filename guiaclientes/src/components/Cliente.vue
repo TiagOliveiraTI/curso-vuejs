aa6dadac5224a2f6581dc83eb4428b788f1d354e
Tiago Oliveira <tiago.oliveira@i9xp.com.br>
2020-10-14 14:03:06 -0300

    Criando os  primeiros componentes

---
+++ guiaclientes/src/components/Cliente.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="div-cliente">
+        <h2>Cliente aqui!</h2>
+        <p>Gerado por componente</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    #div-cliente {
+        color: aquamarine;
+    }
+</style>
---
+++ guiaclientes/src/components/Produto.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <h3>Meu Produto</h3>
+      <input type="text" name="" id="" placeholder="Preço">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
